1a79eea725345d79be92a94f5991d6283a35fee5
Leonardo Guedes <leocguedex@gmail.com>
2021-12-21 17:13:07 -0300

    first commit

---
+++ src/main/java/com/leonardoguedex/pedidos/PedidosCursoApiApplication.java
@@ -0,0 +1,13 @@
+package com.leonardoguedex.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosCursoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosCursoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardoguedex/pedidos/PedidosCursoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoguedex.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosCursoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
