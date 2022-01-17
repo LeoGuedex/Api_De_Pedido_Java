011b41021b8d5eb96d26e72b1af7c6f7c3717482
Leonardo Guedes <leocguedex@gmail.com>
2022-01-17 20:54:13 -0300

    Deletando uma Categoria, com DELETE, criando uma exception para integridade de dados

--- src/main/java/com/leonardoguedex/pedidos/rest/exception/RestExceptionHandler.java
+++ src/main/java/com/leonardoguedex/pedidos/rest/exception/RestExceptionHandler.java
@@ -1,18 +1,31 @@
 package com.leonardoguedex.pedidos.rest.exception;
 
+import com.leonardoguedex.pedidos.exception.DataIntegratyException;
 import com.leonardoguedex.pedidos.exception.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> handlerObjectNotFoundException(ObjectNotFoundException e){
         StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
     }
+
+    @ExceptionHandler(DataIntegratyException.class)
+    public ResponseEntity<StandardError> handlerDataIntegratyException(DataIntegratyException e, HttpServletRequest request){
+        StandardError standardError = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+    }
+
+
+
 
 }
